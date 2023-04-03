f91a4e7fe446d4c1015bda41fbfc83ce1761b243
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2023-04-02 21:41:17 -0300

    api

---
+++ api_pw/src/main/java/io/github/marciojcarvalho/api_pw/ApiPwApplication.java
@@ -0,0 +1,13 @@
+package io.github.marciojcarvalho.api_pw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPwApplication.class, args);
+	}
+
+}
---
+++ api_pw/src/test/java/io/github/marciojcarvalho/api_pw/ApiPwApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marciojcarvalho.api_pw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
