9439d51e2052f5b4e82697b4f74319d0f40d2d83
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2023-04-10 08:40:09 -0300

    salvando um commit no banco

--- api_pw/src/main/java/io/github/marciojcarvalho/api_pw/service/CommitServiceImpl.java
+++ api_pw/src/main/java/io/github/marciojcarvalho/api_pw/service/CommitServiceImpl.java
@@ -1,6 +1,7 @@
 package io.github.marciojcarvalho.api_pw.service;
 
 import io.github.marciojcarvalho.api_pw.dto.CommitDTO;
+import io.github.marciojcarvalho.api_pw.entity.Commit;
 import io.github.marciojcarvalho.api_pw.repository.CommitRepository;
 import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,8 @@ public class CommitServiceImpl implements CommitService{
     private final CommitRepository commitRepository;
     @Override
     public CommitDTO cadastrar(CommitDTO commitDTO) {
-        return null;
+        Commit commit = new Commit();
+        commit = commit.converter(commitDTO);
+        return commitDTO.converter(commitRepository.save(commit));
     }
 }
